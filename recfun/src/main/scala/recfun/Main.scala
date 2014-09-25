75eb191ab86804c2875c2441982fe3e0ea3b3e6e
Federico Aloi <federico.aloi@gmail.com>
2014-09-25 00:43:43 -0300

    Implemented Pascal's triangle - week 1

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -1,7 +1,9 @@
 package recfun
 
-object Main {
-  def main(args: Array[String]) {
+import scala.annotation.tailrec
+
+object Main extends App {
+  override def main(args: Array[String]) {
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
       for (col <- 0 to row)
@@ -13,12 +15,19 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    (c, r) match {
+      case (x, y) if x == y => 1
+      case (0, _) => 1
+      case (x, y) => pascal(x - 1, y - 1) + pascal(x, y -1)
+    }
+  }
 
   /**
    * Exercise 2
    */
   def balance(chars: List[Char]): Boolean = {
+    @tailrec
     def loop(count: Int, chars: List[Char]): Boolean = {
       if (count < 0)
         false
