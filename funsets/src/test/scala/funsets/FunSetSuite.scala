47cd8b30d8d7e4751d06d383e2bd287b2a4946b0
Federico Aloi <federico.aloi@gmail.com>
2014-09-26 16:48:43 -0300

    [W2] Implemented singletonSet

---
+++ funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -0,0 +1,46 @@
+package funsets
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+/**
+ * This class is a test suite for the methods in object FunSets. To run
+ * the test suite, you can either:
+ *  - run the "test" command in the SBT console
+ *  - right-click the file in eclipse and chose "Run As" - "JUnit Test"
+ */
+@RunWith(classOf[JUnitRunner])
+class FunSetSuite extends FunSuite {
+  import FunSets._
+
+  test("contains is implemented") {
+    assert(contains(x => true, 100))
+  }
+
+  trait TestSets {
+    val s1 = singletonSet(1)
+    val s2 = singletonSet(2)
+    val s3 = singletonSet(3)
+  }
+
+  new TestSets {
+    test("singletonSet(1) contains 1") {
+      assert(contains(s1, 1))
+    }
+
+    test("singletonSet(1) doesn't contain 2") {
+      assert(!contains(s1, 2))
+    }
+  }
+
+  ignore("union contains all elements") {
+    new TestSets {
+      val s = union(s1, s2)
+      assert(contains(s, 1), "Union 1")
+      assert(contains(s, 2), "Union 2")
+      assert(!contains(s, 3), "Union 3")
+    }
+  }
+}
